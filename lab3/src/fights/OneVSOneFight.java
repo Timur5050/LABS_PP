5a5d1386b9327c1016cb00c771d1a7f18550022f
timur <stukantimur811@gmail.com>
2024-10-04 16:27:15 +0300

    added logging

--- lab3/src/fights/OneVSOneFight.java
+++ lab3/src/fights/OneVSOneFight.java
@@ -1,16 +1,15 @@
 package fights;
 
 import droids.Droid;
+import logging.ConsoleLogger;
+import logging.FileLogger;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Objects;
 import java.util.Random;
 
 public class OneVSOneFight extends Fight {
-    String filePath = "droids.txt";
-    File file = new File(filePath);
+    ConsoleLogger consoleLogger = new ConsoleLogger();
+    FileLogger fileLogger = new FileLogger();
 
     public OneVSOneFight(Droid droid1, Droid droid2) {
         team1.add(droid1);
@@ -21,18 +20,24 @@ public class OneVSOneFight extends Fight {
     public void fight() {
         Random random = new Random();
 
-        System.out.println("Who starts first? Let's throw monet (0 - first player, 1 - second player)");
+        consoleLogger.logInformation("Who starts first? Let's throw monet (0 - first player, 1 - second player)", "");
+        fileLogger.logInformation("Who starts first? Let's throw monet (0 - first player, 1 - second player)", "");
         Droid droid1 = team1.get(0), droid2 = team2.get(0);
         boolean res = random.nextBoolean();
 
         if (res) {
-            System.out.println("first player starts");
+            consoleLogger.logInformation("first player starts", "");
+            fileLogger.logInformation("first player starts", "");
         } else {
-            System.out.println("second player starts");
+            consoleLogger.logInformation("second player starts", "");
+            fileLogger.logInformation("second player starts", "");
         }
 
         while (true) {
-            System.out.print(COLOR_GREEN + "\nsmall stats : " + droid1.getName() + " hp :  " + droid1.getHealthPoints() + "     |     " + droid2.getName() + " hp : " + droid2.getHealthPoints() + COLOR_RESET);
+            String stats = "\nsmall stats : " + droid1.getName() + " hp :  " + droid1.getHealthPoints() + "     |     " + droid2.getName() + " hp : " + droid2.getHealthPoints();
+            consoleLogger.logInformation(stats, "green");
+            fileLogger.logInformation(stats, "green");
+
             if (res) {
                 if (performStep(droid1, droid2, "first")) break;
                 if (performStep(droid2, droid1, "second")) break;
@@ -44,59 +49,47 @@ public class OneVSOneFight extends Fight {
     }
 
     private boolean performStep(Droid attacker, Droid defender, String player) {
-        System.out.println(COLOR_BLUE + "\n" + player + " step: " + COLOR_RESET);
+        String stepMessage = "\n" + player + " step: ";
+        consoleLogger.logInformation(stepMessage, "blue");
+        fileLogger.logInformation(stepMessage, "blue");
+
         step(attacker, defender, player);
         return localCheck() == 1;
     }
 
     private int localCheck() {
         if (this.GeneralCheck() == 1) {
-            System.out.println(COLOR_PURPLE + "player second won, congrats!!!!" + COLOR_RESET);
-            try (FileWriter writer = new FileWriter(file, true)) {
-                writer.write(COLOR_PURPLE + "player second won, congrats!!!!" + "\n" + COLOR_RESET);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            consoleLogger.logInformation("\nplayer second won, congrats!!!!", "purple");
+            fileLogger.logInformation("\nplayer second won, congrats!!!!\n", "purple");
             return 1;
         }
         if (this.GeneralCheck() == 2) {
-            System.out.println(COLOR_PURPLE + "player first won, congrats!!!!" + COLOR_RESET);
-            try (FileWriter writer = new FileWriter(file, true)) {
-                writer.write(COLOR_PURPLE + "player first won, congrats!!!!" + "\n" + COLOR_RESET);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            consoleLogger.logInformation("\nplayer first won, congrats!!!!", "purple");
+            fileLogger.logInformation("\nplayer first won, congrats!!!!\n", "purple");
             return 1;
         }
         return 0;
     }
 
     private void step(Droid droid1, Droid droid2, String player) {
+        String droidType = droid1.getType();
 
-        if (Objects.equals(droid1.getType(), "attacker")) {
-            System.out.println(COLOR_YELLOW + "droid" + team1.get(0).getName() + " hit " + team2.get(0).getName() + COLOR_RESET);
+        if (Objects.equals(droidType, "attacker")) {
             int damage = droid1.getTotalDamage();
             droid2.receiveDamage(damage);
-            try (FileWriter writer = new FileWriter(file, true)) {
-                writer.write(COLOR_YELLOW + "(" + player + ") droid" + team1.get(0).getName() + " hit " + team2.get(0).getName() + "\n" + COLOR_RESET);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else if (Objects.equals(droid1.getType(), "healer")) {
-            System.out.println(COLOR_YELLOW + "You can not heal bro in one to one fight :(" + COLOR_RESET);
-            try (FileWriter writer = new FileWriter(file, true)) {
-                writer.write(COLOR_YELLOW + "(" + player + ") You can not heal bro in one to one fight :(\n" + COLOR_RESET );
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else if (Objects.equals(droid1.getType(), "reviver")) {
-            System.out.println(COLOR_YELLOW + "You can not revive bro in one to one fight :(" + COLOR_RESET);
-            try (FileWriter writer = new FileWriter(file, true)) {
-                writer.write(COLOR_YELLOW + "(" + player + ") You can not revive bro in one to one fight :(\n"+  COLOR_RESET);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+            String attackMessage = "droid" + droid1.getName() + " hit " + droid2.getName() + " with damage : " + damage + ", now it has : " + droid2.getHealthPoints();
+            consoleLogger.logInformation(attackMessage, "yellow");
+            fileLogger.logInformation(attackMessage, "yellow");
+
+        } else if (Objects.equals(droidType, "healer")) {
+            String healMessage = "You can not heal bro in one to one fight :(";
+            consoleLogger.logInformation(healMessage, "yellow");
+            fileLogger.logInformation(healMessage, "yellow");
+        } else if (Objects.equals(droidType, "reviver")) {
+            String reviveMessage = "You can not revive bro in one to one fight :(";
+            consoleLogger.logInformation(reviveMessage, "yellow");
+            fileLogger.logInformation(reviveMessage, "yellow");
         }
     }
-
 }
